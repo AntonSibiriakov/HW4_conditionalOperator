70653e1e2dabd7b51c5f6bb86e2ad1fd9a681196
ThinkPad <Tonan041@gmail.com>
2022-11-14 17:39:51 +0100

    1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        //Задание №1
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Поздравляю с совершенноолетием ");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, подожди немного");
+        }
+
+        //Задание №2
+
+    }
+}
